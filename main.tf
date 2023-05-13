49733a755871c66722f34262c5fe939c54c45aa1
pankaj620 <59977514+pankaj620@users.noreply.github.com>
2023-05-13 12:33:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,5 +7,8 @@ resource "aws_s3_bucket" "buckets"{
    App="cloud@gmail.com"
    CC="16263"
    SDO="16263"
+   Owner="pankaj.paswan@genpact.com"
+   Supervisor="krishna.singh@genpact.com"
+   Hardening="enabled"
   }
 }
