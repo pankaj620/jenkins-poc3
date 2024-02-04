dab4ddb493ee622bb675baa0b0d2635848268b35
pankaj620 <59977514+pankaj620@users.noreply.github.com>
2024-02-04 17:10:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
 resource "aws_s3_bucket" "buckets"{
  bucket=var.bucket_name
-  
-  tags={
-   Purpose="Testing"
-   Owner="Pankaj"
-   App="cloud@gmail.com"
-   CC="16263"
-   SDO="16263"
-   Owner="pankaj.paswan@adobe.com"
-   Project: "TFE"
-  }
-}
+ tags=var.tags
